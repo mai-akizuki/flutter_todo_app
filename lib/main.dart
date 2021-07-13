149626732e6feebf1484137ab3b206ca28b9b8d0
mai-akizuki <mai0726citron@gmail.com>
2021-07-13 23:37:34 +0900

    Firestoreで複数の値を一気に削除する

--- lib/main.dart
+++ lib/main.dart
@@ -26,17 +26,38 @@ class MainPage extends StatelessWidget {
     return ChangeNotifierProvider<MainModel>(
       create: (_) => MainModel()..getTodoListRealtime(),
       child: Scaffold(
-        appBar: AppBar(
-          title: Text('Todoアプリ'),
-        ),
+        appBar: AppBar(title: Text('Todoアプリ'), actions: [
+          Consumer<MainModel>(builder: (context, model, child) {
+            final isActive = model.checkShouldActiveComplteButton();
+            return ElevatedButton(
+              onPressed: isActive
+                  ? () async {
+                      await model.deleteCheckedItems();
+                    }
+                  : null,
+              child: Text(
+                '完了',
+                style: TextStyle(
+                  color:
+                      isActive ? Colors.white : Colors.white.withOpacity(0.5),
+                ),
+              ),
+            );
+          })
+        ]),
         body: Consumer<MainModel>(
           builder: (context, model, child) {
             final todoList = model.todoList;
             return ListView(
               children: todoList
                   .map(
-                    (todo) => ListTile(
+                    (todo) => CheckboxListTile(
                       title: Text(todo.title),
+                      value: todo.isDone,
+                      onChanged: (bool? value) {
+                        todo.isDone = !todo.isDone;
+                        model.reload();
+                      },
                     ),
                   )
                   .toList(),
--- lib/todo.dart
+++ lib/todo.dart
@@ -3,10 +3,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Todo {
   Todo(DocumentSnapshot doc) {
     final data = doc.data() as Map<String, dynamic>;
+    this.documentReference = doc.reference;
+
     this.title = data['title'];
+
     final Timestamp timestamp = data['createdAt'];
     this.createdAt = timestamp.toDate();
   }
+
   late String title;
   late DateTime createdAt;
+  bool isDone = false;
+  late DocumentReference documentReference;
 }
